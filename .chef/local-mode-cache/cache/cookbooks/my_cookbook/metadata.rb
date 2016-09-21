e16803e8915067b541ca6b33a009201946b68acb
jeyashreedeepika <jeyashreedeepika.s@tcs.com>
2016-09-21 09:29:53 +0000

    comiittted

---
+++ .chef/local-mode-cache/cache/cookbooks/my_cookbook/metadata.rb
@@ -0,0 +1,10 @@
+name 'my_cookbook'
+maintainer 'Jeyashree Deepika'
+maintainer_email 'jeyashreedeepika.s@tcs.com'
+license 'all_rights'
+description 'Installs/Configures my_cookbook'
+long_description 'Installs/Configures my_cookbook'
+version '0.1.0'
+
+depends "apt"
+depends "ntp"
---
+++ .chef/local-mode-cache/cache/cookbooks/my_cookbook/recipes/default.rb
@@ -0,0 +1,4 @@
+
+file "/tmp/local_mode.txt" do
+    content "created by chef client local mode"
+end
